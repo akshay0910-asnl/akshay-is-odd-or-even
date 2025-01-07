6dc3aeb4c775dbeaab4a5f907a8c6ca6e71b15fa
Akshay Kumar <akgunner0910@gmail.com>
2025-01-07 14:50:43 +0530

    Added definition file

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export = isOddOrEven;
+/**
+ * Adds two numbers.
+ * @param {number} n - The number.
+ * @returns {string} Whether the number is even or odd.
+ */
+declare function isOddOrEven(n: number): string;
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * Adds two numbers.
+ * @param {number} n - The number.
+ * @returns {string} Whether the number is even or odd.
+ */
 const isOddOrEven = (n) => {
   if (n % 2 === 0) {
     return 'even';
